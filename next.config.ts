1c38387515f09a6736c5e19fdc6aa67978aaa189
Anthony Mcgarry <antjmcgarry@gmail.com>
2025-04-14 08:17:36 +0100

    fix: ensure assetPrefix is correctly set in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import { type NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
   basePath: "/portfolio",
+  assetPrefix: "/portfolio",
   output: "export",
 };
 console.log("nextConfig", nextConfig);
